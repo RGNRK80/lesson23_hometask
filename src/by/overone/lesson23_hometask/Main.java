5ef095d11da118f406707cd210ba7523a5ab4928
RGNRK80 <armhound@gmail.com>
2021-11-09 14:01:21 +0300

    Initial commit

---
+++ src/by/overone/lesson23_hometask/Main.java
@@ -0,0 +1,37 @@
+package by.overone.lesson23_hometask;
+
+public class Main {
+    public static void main(String[] args) {
+        Num<Float> xNum=new Num(50.15);
+        short x=10;
+        System.out.println(xNum.multiplikate(0.5f));
+        var z=xNum.multiplikate(x);
+        System.out.println(z);
+        short z2;
+        z2 = xNum.division(x);
+        System.out.println(z2);
+        z2=xNum.addTo(x);
+
+        Num<Float> yNum=new Num(4000);
+        Num zNum=xNum.multiplikate(yNum);
+        System.out.println(zNum.getVal().getClass());
+        zNum=xNum.addTo(yNum);
+
+// 2 метод
+        System.out.println("----2----");
+
+        Num<Integer> numInt1=new Num<>(150);
+        System.out.println(numInt1);  //+
+        Num<Integer> numInt2=new Num<>(500);
+        System.out.println(numInt2);  //+
+        Num<Double> numDou1=new Num<>(50.50);
+        System.out.println(numDou1);  //+
+
+        Num<Integer> numInt3=numInt1.sum(numDou1);
+        System.out.println("rezult:  " +  numInt3);
+
+
+
+
+    }
+}
---
+++ src/by/overone/lesson23_hometask/Num.java
@@ -0,0 +1,257 @@
+package by.overone.lesson23_hometask;
+
+public class Num < T extends Number>{
+
+    private T val;
+    public Num() {};
+    public Num(T val) {
+        this.val = val;
+    }
+    public T getVal() {
+        return val;
+    }
+    public void setVal(T val) {
+        this.val = val;
+    }
+
+    public double multiplikate (double x) {
+        var rezult=val.doubleValue() * x;
+        if (rezult>Double.MAX_VALUE || rezult<Double.MIN_VALUE) {throw new ArithmeticException("double overflow");}
+        return (double) rezult;
+    }
+
+    public  float multiplikate (float x) {
+        var rezult=val.doubleValue() * x;
+        if (rezult>Float.MAX_VALUE || rezult<Float.MIN_VALUE) {throw new ArithmeticException("float overflow");}
+        return (float) rezult;
+    }
+    public  int multiplikate (int x) {
+        var rezult= val.doubleValue() * x;
+        if (rezult>Integer.MAX_VALUE || rezult<Integer.MIN_VALUE) {throw new ArithmeticException("integer overflow");}
+        return (int) rezult;
+    }
+    public  long multiplikate (long x) {
+        var rezult= val.doubleValue() * x;
+        if (rezult>Long.MAX_VALUE || rezult<Long.MIN_VALUE) {throw new ArithmeticException("long overflow");}
+        return (long) rezult;
+    }
+    public  short multiplikate (short x) {
+        var rezult=val.doubleValue() * x;
+        if (rezult>Short.MAX_VALUE || rezult<Short.MIN_VALUE) {throw new ArithmeticException("short overflow");}
+        return (short) rezult;
+    }
+    public  byte multiplikate (byte x) {
+        var rezult=val.doubleValue() * x;
+        if (rezult>Byte.MAX_VALUE  || rezult<Byte.MIN_VALUE) {throw new ArithmeticException("byte overflow");}
+        return (byte) rezult;
+    }
+
+    public Num multiplikate (Num x1) {
+        if (x1.val instanceof Long) { Num rez=new Num(multiplikate(x1.val.longValue())); return rez;}
+        if (x1.val instanceof Integer) { Num rez=new Num(multiplikate(x1.val.intValue())); return rez;}
+        if (x1.val instanceof Float) { Num rez=new Num(multiplikate(x1.val.floatValue())); return rez;}
+        if (x1.val instanceof Double) { Num rez=new Num(multiplikate(x1.val.doubleValue())); return rez;}
+        if (x1.val instanceof Byte) { Num rez=new Num(multiplikate(x1.val.byteValue())); return rez;}
+        if (x1.val instanceof Short) { Num rez=new Num(multiplikate(x1.val.shortValue())); return rez;}
+
+        return null;
+    }
+
+    public double division (double x) {
+        var rezult=val.doubleValue() / x;
+        if (rezult>Double.MAX_VALUE  || rezult<Double.MIN_VALUE) {throw new ArithmeticException("double overflow");}
+        return (double) rezult;
+    }
+
+    public  float division (float x) {
+        var rezult=val.doubleValue() / x;
+        if (rezult>Float.MAX_VALUE  || rezult<Float.MIN_VALUE) {throw new ArithmeticException("float overflow");}
+        return (float) rezult;
+    }
+    public  int division (int x) {
+        var rezult= val.doubleValue() / x;
+        if (rezult>Integer.MAX_VALUE || rezult<Integer.MIN_VALUE) {throw new ArithmeticException("integer overflow");}
+        return (int) rezult;
+    }
+    public  long division (long x) {
+        var rezult= val.doubleValue() / x;
+        if (rezult>Long.MAX_VALUE || rezult<Long.MIN_VALUE) {throw new ArithmeticException("long overflow");}
+        return (long) rezult;
+    }
+    public  short division (short x) {
+        var rezult=val.doubleValue() / x;
+        if (rezult>Short.MAX_VALUE || rezult<Short.MIN_VALUE) {throw new ArithmeticException("short overflow");}
+        return (short) rezult;
+    }
+    public  byte division (byte x) {
+        var rezult=val.doubleValue() / x;
+        if (rezult>Byte.MAX_VALUE || rezult<Byte.MIN_VALUE) {throw new ArithmeticException("byte overflow");}
+        return (byte) rezult;
+    }
+
+    public Num division (Num x1) {
+        if (x1.val instanceof Long) { Num rez=new Num(division(x1.val.longValue())); return rez;}
+        if (x1.val instanceof Integer) { Num rez=new Num(division(x1.val.intValue())); return rez;}
+        if (x1.val instanceof Float) { Num rez=new Num(division(x1.val.floatValue())); return rez;}
+        if (x1.val instanceof Double) { Num rez=new Num(division(x1.val.doubleValue())); return rez;}
+        if (x1.val instanceof Byte) { Num rez=new Num(division(x1.val.byteValue())); return rez;}
+        if (x1.val instanceof Short) { Num rez=new Num(division(x1.val.shortValue())); return rez;}
+        return null;
+    }
+
+    public double addTo (double x) {
+        var rezult=val.doubleValue() + x;
+        if (rezult>Double.MAX_VALUE || rezult<Double.MIN_VALUE) {throw new ArithmeticException("double overflow");}
+        return (double) rezult;
+    }
+
+    public  float addTo (float x) {
+        var rezult=val.doubleValue() + x;
+        if (rezult>Float.MAX_VALUE || rezult<Float.MIN_VALUE) {throw new ArithmeticException("float overflow");}
+        return (float) rezult;
+    }
+
+    public  int addTo (int x) {
+        var rezult= val.doubleValue() + x;
+        if (rezult>Integer.MAX_VALUE || rezult<Integer.MIN_VALUE) {throw new ArithmeticException("integer overflow");}
+        return (int) rezult;
+    }
+    public  long addTo (long x) {
+        var rezult= val.doubleValue() + x;
+        if (rezult>Long.MAX_VALUE || rezult<Long.MIN_VALUE) {throw new ArithmeticException("long overflow");}
+        return (long) rezult;
+    }
+    public  short addTo (short x) {
+        var rezult=val.doubleValue() + x;
+        if (rezult>Short.MAX_VALUE || rezult<Short.MIN_VALUE) {throw new ArithmeticException("short overflow");}
+        return (short) rezult;
+    }
+    public  byte addTo (byte x) {
+        var rezult=val.doubleValue() + x;
+        if (rezult>Byte.MAX_VALUE || rezult<Byte.MIN_VALUE) {throw new ArithmeticException("byte overflow");}
+        return (byte) rezult;
+    }
+
+    public Num addTo (Num x1) {
+        if (x1.val instanceof Long) { Num rez=new Num(addTo(x1.val.longValue())); return rez;}
+        if (x1.val instanceof Integer) { Num rez=new Num(addTo(x1.val.intValue())); return rez;}
+        if (x1.val instanceof Float) { Num rez=new Num(addTo(x1.val.floatValue())); return rez;}
+        if (x1.val instanceof Double) { Num rez=new Num(addTo(x1.val.doubleValue())); return rez;}
+        if (x1.val instanceof Byte) { Num rez=new Num(addTo(x1.val.byteValue())); return rez;}
+        if (x1.val instanceof Short) { Num rez=new Num(addTo(x1.val.shortValue())); return rez;}
+        return null;
+    }
+
+    public double subtraction (double x) {
+        var rezult=val.doubleValue() - x;
+        if (rezult>Double.MAX_VALUE || rezult<Double.MIN_VALUE) {throw new ArithmeticException("double overflow");}
+        return (double) rezult;
+    }
+
+    public  float subtraction (float x) {
+        var rezult=val.doubleValue() - x;
+        if (rezult>Float.MAX_VALUE || rezult<Float.MIN_VALUE) {throw new ArithmeticException("float overflow");}
+        return (float) rezult;
+    }
+
+    public  int subtraction (int x) {
+        var rezult= val.doubleValue() - x;
+        if (rezult>Integer.MAX_VALUE || rezult<Integer.MIN_VALUE) {throw new ArithmeticException("integer overflow");}
+        return (int) rezult;
+    }
+    public  long subtraction (long x) {
+        var rezult= val.doubleValue() - x;
+        if (rezult>Long.MAX_VALUE || rezult<Long.MIN_VALUE) {throw new ArithmeticException("long overflow");}
+        return (long) rezult;
+    }
+    public  short subtraction (short x) {
+        var rezult=val.doubleValue() - x;
+        if (rezult>Short.MAX_VALUE || rezult<Short.MIN_VALUE) {throw new ArithmeticException("short overflow");}
+        return (short) rezult;
+    }
+    public  byte subtraction (byte x) {
+        var rezult=val.doubleValue() - x;
+        if (rezult>Byte.MAX_VALUE || rezult<Byte.MIN_VALUE) {throw new ArithmeticException("byte overflow");}
+        return (byte) rezult;
+    }
+
+    public Num subtraction (Num x1) {
+        if (x1.val instanceof Long) { Num rez=new Num(subtraction(x1.val.longValue())); return rez;}
+        if (x1.val instanceof Integer) { Num rez=new Num(subtraction(x1.val.intValue())); return rez;}
+        if (x1.val instanceof Float) { Num rez=new Num(subtraction(x1.val.floatValue())); return rez;}
+        if (x1.val instanceof Double) { Num rez=new Num(subtraction(x1.val.doubleValue())); return rez;}
+        if (x1.val instanceof Byte) { Num rez=new Num(subtraction(x1.val.byteValue())); return rez;}
+        if (x1.val instanceof Short) { Num rez=new Num(subtraction(x1.val.shortValue())); return rez;}
+        return null;
+    }
+
+
+
+    // Исполнение 2
+
+
+
+
+    public Num<T> getCl (T unit, Double rezult) {
+        if (val instanceof Integer) {Num<Integer> rint = new Num(rezult.intValue()); return (Num<T>) rint; }
+        if (val instanceof Long) {Num<Integer> rint = new Num(rezult.longValue());return (Num<T>) rint; }
+        if (val instanceof Short) {Num<Integer> rint = new Num(rezult.shortValue()); return (Num<T>) rint;}
+        if (val instanceof Byte) {Num<Integer> rint = new Num(rezult.byteValue());return (Num<T>) rint; }
+        if (val instanceof Float) {Num<Integer> rint = new Num(rezult.floatValue());return (Num<T>) rint; }
+        if (val instanceof Double) {Num<Integer> rint = new Num(rezult);return (Num<T>) rint;}
+
+        return null;
+    }
+
+
+    public Num<T> sum(Num x1) {
+        Double rez = val.doubleValue() + x1.val.doubleValue();
+        checkToOverflow(val, rez);
+        Num<T> r = getCl(val, rez);
+        return r;
+    }
+
+    public Num<T> mult (Num x1) {
+        Double rez = val.doubleValue() * x1.val.doubleValue();
+        checkToOverflow(val, rez);
+        Num<T> r = getCl(val, rez);
+        return r;
+    }
+    public Num<T> divis (Num x1) {
+        Double rez = val.doubleValue() / x1.val.doubleValue();
+        checkToOverflow(val, rez);
+        Num<T> r = getCl(val, rez);
+        return r;
+    }
+    public Num<T> substr (Num x1) {
+        Double rez = val.doubleValue() - x1.val.doubleValue();
+        checkToOverflow(val, rez);
+        Num<T> r = getCl(val, rez);
+        return r;
+    }
+
+
+    public void checkToOverflow (T unit, Double rezult) {
+        if ((unit instanceof Integer) && (rezult>Integer.MAX_VALUE || rezult<Integer.MIN_VALUE)) {
+            throw new ArithmeticException("integer overflow");}
+        if ((unit instanceof Float) && (rezult>Float.MAX_VALUE || rezult<Float.MIN_VALUE)) {
+            throw new ArithmeticException("integer overflow");}
+        if ((unit instanceof Double) && (rezult>Double.MAX_VALUE || rezult<Double.MIN_VALUE)) {
+            throw new ArithmeticException("integer overflow");}
+        if ((unit instanceof Byte) && (rezult>Byte.MAX_VALUE || rezult<Byte.MIN_VALUE)) {
+            throw new ArithmeticException("integer overflow");}
+        if ((unit instanceof Short) && (rezult>Short.MAX_VALUE || rezult<Short.MIN_VALUE)) {
+            throw new ArithmeticException("integer overflow");}
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Num{" +
+                "val=" + val +
+                '}'+ "Class" + val.getClass();
+    }
+}
+
+
